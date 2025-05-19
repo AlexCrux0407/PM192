b753f2ca66cb09c721b5855f262fdcb6f9f340ca
AlexCrux0407 <130202688+AlexCrux0407@users.noreply.github.com>
2025-05-19 15:28:16 -0600

    ejercicio 2

---
+++ Practica1/Ejercicio2.js
@@ -0,0 +1,11 @@
+//version con function
+//function cuadrado(numero) {
+  //  return numero * numero;
+//}
+
+//version con arrow function
+const cuadrado = (numero) => {return numero * numero;}
+
+console.log(cuadrado(5));
+console.log(cuadrado(10));
+console.log(cuadrado(15));
