802339a783e7df1bf82bd5cf80078b7e8004e149
AlexCrux0407 <130202688+AlexCrux0407@users.noreply.github.com>
2025-06-23 15:11:21 -0600

    ImgBg

---
+++ ImgBg/App.js
@@ -0,0 +1,41 @@
+import { StatusBar } from 'expo-status-bar';
+import { ImageBackground, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+const fondoBienvenida = () => {
+  return(
+    <ImageBackground
+      source={require('./assets/fondo.jpg')} style={styles.fondo}>
+      <View style={styles.contenido}>
+        <Text style={styles.contenido}> Bienvenido a mi app</Text>
+      </View>
+    </ImageBackground>
+  )
+};
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <fondoBienvenida />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  fondo:{
+    flex: 1
+  },
+  contenido: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.4)',
+  },
+  titulo: {
+    fontSize: 28,
+    color: 'white',
+    fontWeight: 'bold',
+
+  }
+});
